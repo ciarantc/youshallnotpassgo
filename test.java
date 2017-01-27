9f649340f0ae817b831129838e5b8c155a75d179
Coswar <callumoshea@hotmail.com>
2017-01-27 12:22:08 +0000

    java program

--- test.java
+++ test.java
@@ -1,1 +1,19 @@
-test123456
+package practical2;
+
+import javax.swing.JOptionPane;
+
+public class circlearea {
+
+	public static void main(String[] args)
+	{
+		String radius;
+		Double numrad;
+		Double area;
+		radius = JOptionPane.showInputDialog(null,"Enter the radius of a circle:");
+		numrad = Double.parseDouble(radius);
+		area = (numrad*numrad*3.1416);
+		JOptionPane.showMessageDialog(null,"Circle of radius " +numrad + " is " + area);
+		System.exit(0);
+	}
+
+}
